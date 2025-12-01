ddb7dd585213348c695f8385bc84c1541202c505
RenatoLT <re.lavanderos@duocuc.cl>
2025-12-01 00:36:18 -0300

    HashRouters

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import './assets/css/style.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './assets/js/SmoothScroll.js';
 import { AdminProvider } from './components/AdminContext.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AdminProvider>
       <App />
     </AdminProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
