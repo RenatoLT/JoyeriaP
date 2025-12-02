6ec260e2573ffca3b296d41c8e53c3ef2692867a
RenatoLT <re.lavanderos@duocuc.cl>
2025-12-01 23:05:44 -0300

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://3.141.164.49:8080/api/v1', // La URL de tu Spring Boot
+    baseURL: 'https://rheumic-faddishly-merrie.ngrok-free.dev/api/v1', // La URL de tu Spring Boot
     headers: {
         'Content-Type': 'application/json'
     }
